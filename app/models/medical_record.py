4f82a04c4bf2aeb2308b5103ee85b85cb62e7a9a
Juan Isaza <juanisaza@Juans-Mac-mini.local>
2025-02-25 21:52:49 -0500

    feat(record): updates fields

--- app/models/medical_record.py
+++ app/models/medical_record.py
@@ -1,5 +1,5 @@
 from .base import Base
-from sqlalchemy import Column, Integer, String, Boolean, Text, ForeignKey
+from sqlalchemy import Column, Integer, String, Boolean, Text, ForeignKey, Date, Numeric
 
 
 class MedicalRecord(Base):
@@ -14,6 +14,9 @@ class MedicalRecord(Base):
     cirugias = Column(Boolean, nullable=False)
     discapacidad = Column(Boolean, nullable=False)
     emer_medica = Column(Text, nullable=False)
+    eps = Column(Text, nullable=True)
+    fecha_ingreso = Column(Date, nullable=True)
+    frecuencia_cardiaca = Column(Numeric, nullable=False)
     historial_cirugias = Column(Text)
     id_historiaclinica = Column(Integer, primary_key=True, autoincrement=True)
     id_usuario = Column(Integer, ForeignKey("Usuarios.id_usuario"), nullable=False)
@@ -22,7 +25,11 @@ class MedicalRecord(Base):
     motivo_ingreso = Column(Text)
     observ_dietaEspecial = Column(Text)
     observ_otrasalergias = Column(Text)
+    observaciones_iniciales = Column(Text)
+    peso = Column(Numeric)
+    presion_arterial = Column(Numeric)
     sustanciaspsico = Column(Boolean, nullable=False)
     tabaquismo = Column(Boolean, nullable=False)
     telefono_emermedica = Column(String(17))
+    temperatura_corporal = Column(Numeric)
     tipo_sangre = Column(String(), nullable=False)
