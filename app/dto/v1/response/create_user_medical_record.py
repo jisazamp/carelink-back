e9e27f37966139b91aefd4727f7f5d82d81fa5b4
Juan Isaza <juanisaza@Juans-MacBook-Pro.local>
2025-03-01 00:03:37 -0500

    feat(conditions): adds special conditions fields

--- app/dto/v1/response/create_user_medical_record.py
+++ app/dto/v1/response/create_user_medical_record.py
@@ -11,12 +11,13 @@ class CreateUserMedicalRecordResponseDTO(BaseModel):
     altura: int
     apariencia_personal: str
     cafeina: bool
-    cirugias: bool
+    cirugias: str | None
     comunicacion_no_verbal: str
     comunicacion_verbal: str
     continencia: bool
     cuidado_personal: str
-    discapacidad: bool
+    dieta_especial: str | None
+    discapacidades: str | None
     emer_medica: str
     eps: str
     estado_de_animo: str
@@ -24,13 +25,15 @@ class CreateUserMedicalRecordResponseDTO(BaseModel):
     frecuencia_cardiaca: float
     historial_cirugias: str
     id_usuario: int
+    limitaciones: str | None
     maltratado: bool
     maltrato: bool
-    medicamentos_alergia: str
+    medicamentos_alergia: str | None
     motivo_ingreso: str
     observ_dietaEspecial: str
     observ_otrasalergias: str
     observaciones_iniciales: str
+    otras_alergias: str | None
     peso: float
     presion_arterial: float
     sustanciaspsico: bool
