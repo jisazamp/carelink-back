0a4368ca15f0d7bac2ffd390f4dbefe247b24424
Juan Isaza <juanisaza@Juans-Mac-mini.local>
2025-06-03 21:05:30 -0500

    create payments

--- app/dto/v1/response/payment_method.py
+++ app/dto/v1/response/payment_method.py
@@ -1,9 +1,22 @@
+from datetime import date
 from pydantic import BaseModel
 
 
 class PaymentMethodResponseDTO(BaseModel):
     id_metodo_pago: int
     nombre: str | None
+
+    class Config:
+        orm_mode = True
+
+
+class PaymentResponseDTO(BaseModel):
+    id_pago: int
+    id_factura: int
+    id_metodo_pago: int
+    id_tipo_pago: int
+    fecha_pago: date
+    valor: float
 
     class Config:
         orm_mode = True
